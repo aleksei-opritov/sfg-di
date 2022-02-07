aae20fd7e29f99b0ad08beb16f8c7180c70bc52c
aleksei-opritov <hutte1spb@gmail.com>
2022-02-07 20:04:20 +0300

    Revert "Changes regarding Spring profiles"
    This reverts commit 406eb2f1ae0779cd0b6e93c1b30ea86afbd0d2e0.

--- src/main/java/guru/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/CatPetService.java
@@ -1,13 +1,11 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/28/19.
  */
-@Profile({"cat"})
-@Service("petService")
+@Service("cat")
 public class CatPetService implements PetService {
     @Override
     public String getPetType() {
